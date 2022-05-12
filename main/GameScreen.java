b3aeee3e3bbdbc52bca45b94c72a93175030d605
suriani <73538532+serisuriani@users.noreply.github.com>
2022-05-12 03:44:38 -0700

    Update GameScreen.java
    randomize colors to fill window

--- main/GameScreen.java
+++ main/GameScreen.java
@@ -1,18 +1,34 @@
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 import javax.swing.JPanel;
 
 public class GameScreen extends JPanel {
 
-    public GameScreen() {
+    private Random random;
 
+    public GameScreen() {
+        random = new Random();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(Color.MAGENTA);
-        g.fillRect(50, 50, 100, 100);
+        for (int y = 0; y < 20; y++) {
+            for (int x = 0; x < 20; x++) {
+                g.setColor(getRndColor());
+                g.fillRect(x * 32, y * 32, 32, 32);
+            }
+
+        }
+    }
+
+    private Color getRndColor() {
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+
+        return new Color(r,g,b);
     }
 }
