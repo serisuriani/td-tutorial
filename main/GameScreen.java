9386b3bdc13e4c588f1e3452b82cc8b36438437e
suriani <73538532+serisuriani@users.noreply.github.com>
2022-05-12 04:27:23 -0700

    Update GameScreen.java

--- main/GameScreen.java
+++ main/GameScreen.java
@@ -2,33 +2,33 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
+import java.awt.image.BufferedImage;
 import javax.swing.JPanel;
 
 public class GameScreen extends JPanel {
 
     private Random random;
+    private BufferedImage img;
 
-    public GameScreen() {
+    public GameScreen(BufferedImage img) {
+        this.img = img;
         random = new Random();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        for (int y = 0; y < 20; y++) {
-            for (int x = 0; x < 20; x++) {
-                g.setColor(getRndColor());
-                g.fillRect(x * 32, y * 32, 32, 32);
-            }
+        g.drawImage(img, 0, 0, null);
 
-        }
     }
 
     private Color getRndColor() {
         int r = random.nextInt(256);
-        int g = random.nextInt(256);
+
         int b = random.nextInt(256);
 
-        return new Color(r,g,b);
+        int g = random.nextInt(256);
+
+        return new Color(r, g, b);
     }
 }
