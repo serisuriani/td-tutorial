474ce7213508c4b5466868a19b38cd52b66671a8
suriani <73538532+serisuriani@users.noreply.github.com>
2022-05-12 04:27:26 -0700

    Update Game.java

--- main/Game.java
+++ main/Game.java
@@ -1,18 +1,35 @@
-
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Game extends JFrame {
 
     private GameScreen gameScreen;
 
+    private BufferedImage img;
+
     public Game() {
 
-        setSize(400, 400);
-        setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        importImg();
+
+        setSize(640, 640);
+
         setLocationRelativeTo(null);
-        gameScreen = new GameScreen();
+        gameScreen = new GameScreen(img);
         add(gameScreen);
+        setVisible(true);
+    }
+
+    private void importImg() {
+
+        InputStream is = getClass().getResourceAsStream("/spriteatlas.png");
+
+        try {
+            img = ImageIO.read(is);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
