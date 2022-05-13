35d3a8e7d8dc179abe3ecf237b5b7a9eecc87e15
suriani <73538532+serisuriani@users.noreply.github.com>
2022-05-13 13:39:34 -0700

    Update GameScreen.java

--- main/GameScreen.java
+++ main/GameScreen.java
@@ -1,5 +1,4 @@
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 import java.util.ArrayList;
@@ -10,9 +9,6 @@ public class GameScreen extends JPanel {
 
     private Random random;
     private BufferedImage img;
-
-    private int frames;
-    private long lastTime;
 
     private ArrayList<BufferedImage> sprites = new ArrayList<>();
 
@@ -41,21 +37,10 @@ public class GameScreen extends JPanel {
             }
         }
 
-        callFPS();
-
         // g.drawImage(sprites.get(8), 0, 0, null);
 
         // BufferedImage i = img.getSubimage(32*9,32,32,32);
         // g.drawImage(i,0,0,null);
-    }
-
-    private void callFPS() {
-        frames++;
-        if (System.currentTimeMillis() - lastTime >= 1000) {
-            System.out.println("FPS : " + frames);
-            frames = 0;
-            lastTime = System.currentTimeMillis();
-        }
     }
 
     private int getRndInt() {
@@ -63,13 +48,4 @@ public class GameScreen extends JPanel {
 
     }
 
-    private Color getRndColor() {
-        int r = random.nextInt(256);
-
-        int b = random.nextInt(256);
-
-        int g = random.nextInt(256);
-
-        return new Color(r, g, b);
-    }
 }
