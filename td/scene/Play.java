2340f326030d4e6b43d50b62f6144beab84dce5b
suriani <73538532+serisuriani@users.noreply.github.com>
2022-05-13 20:58:47 -0700

    Create Play.java

---
+++ td/scene/Play.java
@@ -0,0 +1,19 @@
+package scene;
+
+import java.awt.Graphics;
+
+import main.Game;
+
+public class Play extends GameScene implements SceneMethods {
+
+    public Play(Game game) {
+        super(game);
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+
+}
