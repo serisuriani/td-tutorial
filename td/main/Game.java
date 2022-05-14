52fee4eeab8a1f2f6dd406ab63f1e6ee3796984c
suriani <73538532+serisuriani@users.noreply.github.com>
2022-05-13 20:58:26 -0700

    Update Game.java

--- td/main/Game.java
+++ td/main/Game.java
@@ -4,12 +4,14 @@ import javax.swing.JFrame;
 
 import inputs.KeyboardListener;
 import inputs.MyMouseListener;
+import scene.Menu;
+import scene.Play;
+import scene.Settings;
 
 public class Game extends JFrame implements Runnable {
 
     private GameScreen gameScreen;
-
-    private int updates;
+    private Thread gameThread;
 
     private final double FPS_SET = 120.0;
     private final double UPS_SET = 60.0;
@@ -17,19 +19,30 @@ public class Game extends JFrame implements Runnable {
     private MyMouseListener myMouseListener;
     private KeyboardListener keyboardListener;
 
-    private Thread gameThread;
+    // Classes
+
+    private Render render;
+    private Menu menu;
+    private Play play;
+    private Settings settings;
 
     public Game() {
 
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        gameScreen = new GameScreen(this);
+        initClasses();
         add(gameScreen);
-
         pack();
         setVisible(true);
 
+    }
+
+    private void initClasses() {
+        render = new Render(this);
+        gameScreen = new GameScreen(this);
+        menu = new Menu(this);
+        play = new Play(this);
+        settings = new Settings(this);
     }
 
     private void initInput() {
@@ -49,11 +62,6 @@ public class Game extends JFrame implements Runnable {
         };
 
         gameThread.start();
-    }
-
-    private void updateGame() {
-        updates++;
-        // System.out.println("Game Updated:)");
     }
 
     public static void main(String[] args) {
@@ -89,7 +97,6 @@ public class Game extends JFrame implements Runnable {
             }
             // updates
             if (now - lastUpdate >= timePerUpdate) {
-                updateGame();
                 lastUpdate = now;
                 updates++;
             }
@@ -103,6 +110,23 @@ public class Game extends JFrame implements Runnable {
 
         }
 
+    }
+    // Getter and setters
+
+    public Render getRender() {
+        return render;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public Play getPlay() {
+        return play;
+    }
+
+    public Settings getSettings() {
+        return settings;
     }
 
 }
