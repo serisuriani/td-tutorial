d61abf14e3040c65b7de523b1f2dfbd7ca87a120
suriani <73538532+serisuriani@users.noreply.github.com>
2022-05-13 20:58:52 -0700

    Create Settings.java

---
+++ td/scene/Settings.java
@@ -0,0 +1,19 @@
+package scene;
+
+import java.awt.Graphics;
+
+import main.Game;
+
+public class Settings extends GameScene implements SceneMethods {
+
+    public Settings(Game game) {
+        super(game);
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+
+}
