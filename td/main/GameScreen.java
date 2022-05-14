2dccbd76f2a4923836ebaa3faebd084c03e0273b
suriani <73538532+serisuriani@users.noreply.github.com>
2022-05-13 20:58:29 -0700

    Update GameScreen.java

--- td/main/GameScreen.java
+++ td/main/GameScreen.java
@@ -1,3 +1,4 @@
+package main;
 
 import java.awt.Graphics;
 import java.awt.Dimension;
@@ -7,12 +8,9 @@ public class GameScreen extends JPanel {
 
     private Game game;
     private Dimension size;
-    private Render render;
 
     public GameScreen(Game game) {
         this.game = game;
-
-        render = new Render(this);
 
         size = new Dimension(640, 640);
         setMinimumSize(size);
@@ -24,7 +22,7 @@ public class GameScreen extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        render.render(g);
+        game.getRender().render(g);
 
     }
 
