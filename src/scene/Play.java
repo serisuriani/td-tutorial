6cef2e1ae0f8516b8fccd3a2b57a7e9d7e7f20a3
suriani <73538532+serisuriani@users.noreply.github.com>
2022-05-14 21:14:18 +0800

    Update Play.java

--- src/scene/Play.java
+++ src/scene/Play.java
@@ -1,21 +1,40 @@
 package scene;
 
 import java.awt.Color;
-import java.awt.Graphics;
 
+import helper.LevelBuild;
 import main.Game;
+import managers.TileManager;
+import java.awt.Graphics;
 
 public class Play extends GameScene implements SceneMethods {
 
+    private int[][] lvl;
+    private TileManager tileManager;
+
     public Play(Game game) {
         super(game);
+
+        lvl = LevelBuild.getLevelData();
+        tileManager = new TileManager();
+
+        // lvl
+        // tilemanager
 
     }
 
     @Override
     public void render(Graphics g) {
-        g.setColor(Color.BLUE);
-        g.fillRect(0, 0, 640, 640);
+
+        for (int y = 0; y < lvl.length; y++) {
+            for (int x = 0; x < lvl[y].length; x++) {
+
+                int id = lvl[y][x];
+                g.drawImage(tileManager.getSprite(id), x * 32, y * 32, null);
+
+            }
+
+        }
 
     }
 
