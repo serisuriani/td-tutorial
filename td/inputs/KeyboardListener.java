4c5ffcdf995ce40d9cb1d2176df8e364f50a9c8e
suriani <73538532+serisuriani@users.noreply.github.com>
2022-05-13 20:30:48 -0700

    Create KeyboardListener.java

---
+++ td/inputs/KeyboardListener.java
@@ -0,0 +1,30 @@
+package inputs;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardListener implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_A)
+            System.out.println("A is pressed");
+
+        else if (e.getKeyCode() == KeyEvent.VK_B)
+            System.out.println("B is pressed");
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
