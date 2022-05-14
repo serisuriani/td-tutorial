c54d1502028b1b714a13a8f583d6c0027af2e1be
suriani <73538532+serisuriani@users.noreply.github.com>
2022-05-13 20:58:45 -0700

    Create Menu.java

---
+++ td/scene/Menu.java
@@ -0,0 +1,19 @@
+package scene;
+
+import java.awt.Graphics;
+
+import main.Game;
+
+public class Menu extends GameScene implements SceneMethods {
+
+    public Menu(Game game) {
+        super(game);
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+
+}
