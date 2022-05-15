2f1dc8fa3abfb11a748021ebdb4f1d9d8371d9e1
suriani <73538532+serisuriani@users.noreply.github.com>
2022-05-15 12:33:15 +0800

    Update GameScreen.java

--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -4,19 +4,40 @@ import java.awt.Graphics;
 import java.awt.Dimension;
 import javax.swing.JPanel;
 
+import inputs.KeyboardListener;
+import inputs.MyMouseListener;
+
 public class GameScreen extends JPanel {
 
     private Game game;
     private Dimension size;
+    private MyMouseListener myMouseListener;
+    private KeyboardListener keyboardListener;
 
     public GameScreen(Game game) {
         this.game = game;
 
+        setPanel();
+
+    }
+
+    public void initInput() {
+        myMouseListener = new MyMouseListener(game);
+        keyboardListener = new KeyboardListener();
+
+        addMouseListener(myMouseListener);
+        addMouseMotionListener(myMouseListener);
+        addKeyListener(keyboardListener);
+
+        requestFocus();
+
+    }
+
+    private void setPanel() {
         size = new Dimension(640, 640);
         setMinimumSize(size);
         setPreferredSize(size);
         setMaximumSize(size);
-
     }
 
     public void paintComponent(Graphics g) {
