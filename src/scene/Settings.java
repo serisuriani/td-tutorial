ef815fee1d128a785f7644ca259c10acb9735c44
suriani <73538532+serisuriani@users.noreply.github.com>
2022-05-15 12:33:28 +0800

    Update Settings.java

--- src/scene/Settings.java
+++ src/scene/Settings.java
@@ -18,4 +18,9 @@ public class Settings extends GameScene implements SceneMethods {
         g.fillRect(0, 0, 640, 640);
     }
 
+    @Override
+    public void mouseClicked(int x, int y) {
+
+    }
+
 }
