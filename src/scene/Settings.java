f176f2a686880aa64ba0d0eeb805a854f46660fb
suriani <73538532+serisuriani@users.noreply.github.com>
2022-05-15 21:03:37 +0800

    Update Settings.java
    add QUIT button on SETTINGS scene

--- src/scene/Settings.java
+++ src/scene/Settings.java
@@ -4,22 +4,45 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 import main.Game;
+import userInterface.MyButton;
+import static main.GameStates.*;
 
 public class Settings extends GameScene implements SceneMethods {
 
+    private MyButton bQuit;
+    public Object mouseClicked;
+
     public Settings(Game game) {
         super(game);
+
+        initButton();
+
+    }
+
+    private void initButton() {
+        bQuit = new MyButton("Quit", 30, 30, 100, 30);
+
+    }
+
+    private void drawButtons(Graphics g) {
+        bQuit.draw(g);
 
     }
 
     @Override
     public void render(Graphics g) {
-        g.setColor(Color.GREEN);
+        g.setColor(Color.WHITE);
         g.fillRect(0, 0, 640, 640);
+
+        drawButtons(g);
     }
 
     @Override
     public void mouseClicked(int x, int y) {
+        if (bQuit.getBounds().contains(x, y)) {
+            setGameStates(MENU);
+
+        }
 
     }
 
